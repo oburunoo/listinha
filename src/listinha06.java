368a935704754f37801334dc6c3e060e7b98e9f4
brunomonteirocosta <guardados.bruno@gmail.com>
2022-04-06 12:00:15 -0300

    Resultado Exercício 06

---
+++ src/listinha06.java
@@ -0,0 +1,23 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class listinha06 {
+    private final static double PORCENTAGEM_COMISSAO = 0.15;
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("digite o nome do vendedor: ");
+        String nomeVendedor = sc.nextLine();
+        System.out.println("Informe o salário fixo: ");
+        double salarioFixo = sc.nextDouble();
+        System.out.println("Valor em vendas: ");
+        double vendas = sc.nextDouble();
+        double comissao = PORCENTAGEM_COMISSAO * vendas;
+        double r = salarioFixo + comissao;
+
+        DecimalFormat four = new DecimalFormat("#0.00");
+        System.out.println("TOTAL = R$ " + four.format(r));
+    }
+}
